57f84979d6cba3f26ccd4312e3fe3cc1c211189f
Halton Bagley <haltonba94@gmail.com>
2020-01-03 17:35:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,6 +21,12 @@ def monopoly_with_third_tier
         two_pieces_owned: 50,
         three_pieces_owned: 100,
         four_pieces_owned: 200
+      },
+      names: {
+        reading_railroad: {},
+        pennsylvania_railroad: {},
+        reading_railroad: {},
+        shortline_railroad: {}
       }
     }
   }
