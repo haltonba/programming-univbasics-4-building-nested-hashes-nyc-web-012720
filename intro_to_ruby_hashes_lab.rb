3679c01c4c1d0be3c933c0b18eb203e82cda421f
Halton Bagley <haltonba94@gmail.com>
2020-01-03 17:25:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,10 @@ def monopoly_with_second_tier
   # complexity.
   
   monopoly = {
-    railroads: {}
+    railroads: {
+      pieces:
+    }
   }
-  monopoly[:railroads][:pieces]
 end
 
 def monopoly_with_third_tier
